2db3e86ee38c86a14ebdf0092c01d4b82b5f401b
Mason <mdp220002@utdallas.edu>
2025-11-22 22:26:08 -0600

    another another push

--- src/main/java/FlightPlannerMain.java
+++ src/main/java/FlightPlannerMain.java
@@ -2,7 +2,7 @@
  * Main application entry point. Delegates control to the Driver class
  * to ensure minimization of code in main (OO Requirement).
  */
-public class FlightPlannerApp {
+public class FlightPlannerMain {
 
     public static void main(String[] args) {
         // Instantiate the controller and run the application logic
